71873ce8db30f83fd6578a4f9f868de92f450c42
Farukh-builder <farukhtukhtamishev1@gmail.com>
2025-09-23 15:11:09 +0900

    feat: create getallMembersByAdmin and UpdateMemberByAdmin GraphQL APIs

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -19,6 +19,7 @@ export class MemberResolver {
            return this.memberService.login(input);
     }
 
+// Authenticated
     @Mutation(() => String)
     public async updateMember(): Promise<string> {
         console.log('Mutation: updateMember');
@@ -31,6 +32,23 @@ export class MemberResolver {
         console.log('Query: getMember');
         return this.memberService.getMember()
     }
+
+
+    /** ADMIN */
+
+// Authorization: ADMIN
+
+    @Mutation(() => String)
+    public async getAllMembersByAdmin(): Promise<string> {
+        return this.memberService.getAllMembersByAdmin();
+    }
+
+    @Mutation(() => String)
+    public async updateMemberByAdmin(): Promise<string> {
+        console.log('Mutation: updateMemberByAdmin');
+        return this.memberService.updateMemberByAdmin();
+    }
+
 
     
 }
