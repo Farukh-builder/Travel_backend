a3dcdeeb49fd1222b6ef81f60b8ff1a00f511134
Farukh-builder <farukhtukhtamishev1@gmail.com>
2025-10-01 22:06:10 +0900

    feat: develop boardArticle realted Apis

--- apps/nestar-api/src/components/board-article/board-article.module.ts
+++ apps/nestar-api/src/components/board-article/board-article.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
 import { BoardArticleResolver } from './board-article.resolver';
 import { BoardArticleService } from './board-article.service';
-import { Mongoose } from 'mongoose';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Property } from '../../libs/dto/property/property';
-import PropertySchema from '../../schemas/Property.model';
 import BoardArticleSchema from '../../schemas/BoardArticle.model';
 import { AuthModule } from '../auth/auth.module';
 import { MemberModule } from '../member/member.module';
--- apps/nestar-api/src/libs/enums/common.enum.ts
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -4,7 +4,7 @@ export enum Message {
     SOMETHING_WENT_WRONG = "Something went wrong!",
     NO_DATA_FOUND = "No data is found!",
     CREATE_FAILED = "Created is failed",
-    UPDATE_FAILED = "Created is failed",
+    UPDATE_FAILED = "Update is failed",
     REMOVE_FAILED = "Remove failed",
     UPLOAD_FAILED = "Upload failed",
     BAD_REQUEST = "Bad Request",
