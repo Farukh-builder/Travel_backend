e702c5ea5af768b0bcb8c96c887a178a1575dbaf
Farukh-builder <farukhtukhtamishev1@gmail.com>
2025-10-05 00:02:26 +0900

    feat: develop subscribe unsubscribe and getMemberFollowings, getMemberFollowers graphQL api

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -19,6 +19,7 @@ export const availableCommentSorts = ['createdAt', 'updatedAt']
  /**  IMAGE CONFIGURATION */
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
+import { from } from 'rxjs';
 
 export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
 export const getSerialForImage = (filename: string) => {
@@ -28,7 +29,7 @@ export const getSerialForImage = (filename: string) => {
 
 export const shapeIntoMongoObjectId = (target: any) => {
     return typeof target === 'string' ? new ObjectId(target) : target;
-}
+};
 
 export const lookupMember = {
 	$lookup: {
@@ -37,4 +38,22 @@ export const lookupMember = {
 		foreignField: '_id',
 		as: 'memberData'
 	}
-}
+};
+
+export const lookupFollowingData = {
+	$lookup: {
+		from: 'members',
+		localField: 'followingId',
+		foreignField: '_id',
+		as: 'followingData'
+	},
+};
+
+export const lookupFollowerData = {
+	$lookup: {
+		from: 'members',
+		localField: 'followerId',
+		foreignField: '_id',
+		as: 'followerData'
+	},
+};
